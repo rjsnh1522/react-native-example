e55cdab9eb33271e265f0f8d6c3035c134d166ff
pawan <rjsnh1522@gmail.com>
2024-11-16 10:56:34 +0530

    Clean

---
+++ app/_layout.js
@@ -0,0 +1,11 @@
+import { Stack } from 'expo-router/stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+
+export default function Layout() {
+  return (
+    <Stack>
+        <Stack.Screen name="/" options={{ headerShown: false }} />
+      </Stack>
+  );
+}
---
+++ app/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const IndexPage = () => {
+  return (
+    <View>
+      <Text>IndexPage</Text>
+    </View>
+  )
+}
+
+export default IndexPage
