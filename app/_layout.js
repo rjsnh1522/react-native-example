b32293f81b5cb63fef3d94542231b29ef5e58efe
pawan <rjsnh1522@gmail.com>
2024-11-16 21:11:57 +0530

    added poppins font from google

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,42 @@
 import { Stack } from 'expo-router/stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Drawer } from 'expo-router/drawer';
+import {useFonts, 
+  Poppins_400Regular, 
+  Poppins_600SemiBold, 
+  Poppins_700Bold,
+  Poppins_200ExtraLight, 
+  Poppins_100Thin, 
+  Poppins_300Light, 
+  Poppins_500Medium, 
+  Poppins_900Black, Poppins_800ExtraBold} from "@expo-google-fonts/poppins"
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from 'react';
+
+SplashScreen.preventAutoHideAsync()
 
 export default function Layout() {
+  const [loaded, error] = useFonts({
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_200ExtraLight,
+    Poppins_100Thin, 
+    Poppins_300Light,
+    Poppins_500Medium,
+    Poppins_900Black,
+    Poppins_800ExtraBold
+  })
+  console.log(loaded)
+  useEffect(()=>{
+    if (loaded){
+      SplashScreen.hideAsync();
+    }
+  }, [loaded])
+
   return (
     <Stack>
-        <Stack.Screen name="/" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: true }} />
       </Stack>
   );
 }
--- app/index.js
+++ app/index.js
@@ -1,10 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+
 const IndexPage = () => {
+
   return (
     <View>
-      <Text>IndexPage</Text>
+      <Text style={{fontFamily: "Poppins_100Thin", fontSize: 43}}>Some lorem data to check font...</Text>
+      <Text style={{fontFamily: "Poppins_200ExtraLight", fontSize: 43}}>Some lorem data to check font...</Text>
+      <Text style={{fontFamily: "Poppins_300Light", fontSize: 43}}>Some lorem data to check font...</Text>
+      <Text style={{fontFamily: "Poppins_400Regular", fontSize: 43}}>Some lorem data to check font...</Text>
+      <Text style={{fontFamily: "Poppins_500Medium", fontSize: 43}}>IndexPage...</Text>
+      <Text style={{fontFamily: "Poppins_600SemiBold", fontSize: 43}}>IndexPage...</Text>
+      <Text style={{fontFamily: "Poppins_700Bold", fontSize: 43}}>IndexPage...</Text>
+      <Text style={{fontFamily: "Poppins_800ExtraBold", fontSize: 43}}>IndexPage...</Text>
+      <Text style={{fontFamily: "Poppins_900Black", fontSize: 43}}>IndexPage...</Text>
     </View>
   )
 }
